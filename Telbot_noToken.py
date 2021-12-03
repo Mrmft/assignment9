014c54c0493f2f2367b7615fe2cfb246eefb032b
Mrmft <91953370+Mrmft@users.noreply.github.com>
2021-12-03 10:06:17 +0330

    Update Telbot_noToken.py

--- Telbot_noToken.py
+++ Telbot_noToken.py
@@ -1,7 +1,8 @@
+from random import randint
 import telebot
-from telebot.types import Voice
 from gtts import gTTS
 import qrcode
+import jdatetime
 
 bot = telebot.TeleBot("TOKEN", parse_mode=None) 
 
@@ -26,20 +27,42 @@ help_text= """
 #PRINT MENU  
   
 @bot.message_handler(commands=['help'])
-def send_welcome(message):
+def send_menu(message):
 	bot.reply_to(message, help_text)
 
 #####################
 
 @bot.message_handler(commands=['game'])
-def send_welcome(message):
-	bot.reply_to(message, help_text)
+def play_game(message):
+	global my_random_number 
+	my_random_number =  str(randint(0 , 31))
+	user_number= bot.send_message(message.chat.id , 'من یک عدد فرضی بین 0 تا 30 انتخاب میکنم و شما باید اون عدد رو حدس بزنید.')	
+	bot.register_next_step_handler(user_number , rand_game)
 
-#####################
+#####  NUMBER_GUESSING_GAME
+
+def rand_game(user_number):
+    if user_number.text == my_random_number:
+        bot.send_message(user_number.chat.id , 'هوورررررا شما برنده شدید!!!!!!')
+    elif user_number.text > my_random_number:
+        user_number_new = bot.send_message(user_number.chat.id , 'یه عدد کمتر پیشنهاد بده')
+        bot.register_next_step_handler(user_number_new , rand_game)
+    elif user_number.text < my_random_number:
+        user_number_new = bot.send_message(user_number.chat.id , 'یه عدد بیشتر پیشنهاد بده')
+        bot.register_next_step_handler(user_number_new , rand_game)
+        
+
+####################+
+
 
 @bot.message_handler(commands=['age'])
-def send_welcome(message):
-	bot.reply_to(message, help_text) 
+def send_age(message):
+    bot.send_message(message.chat.id, 'لطفا سن خودت رو در قالب زیر بفرس تا بهت بگم چند سالته!!! \n yyyy/mm/dd')
+    bot.register_next_step_handler(message , find_age)
+
+def find_age(message):
+    x = jdatetime.date.today().split('-')
+    print(x , type(x))
 
 #####################
 
@@ -60,13 +83,32 @@ def Txt2Voice(voicemessage):
 
 ##################
 
-# @bot.message_handler(commands=['max'])
-# def send_welcome(message):
-# 	bot.reply_to(message, help_text)
+@bot.message_handler(commands=['max'])
+def send_max_number(message):
+	integer_string= bot.send_message(message.chat.id, 'یک رشته از اعداد به این صورت وارد نمایید: \n 1,2,3,4,5,6')
+	bot.register_next_step_handler(integer_string, find_max)
 
-# @bot.message_handler(commands=['argmax'])
-# def send_welcome(message):
-# 	bot.reply_to(message, help_text)
+#### FIND_MAX_NUMBER
+
+def find_max(numbers_string):
+    max_number = max(numbers_string.text.split(','))
+    bot.send_message(numbers_string.chat.id , max_number)
+    
+##################
+
+@bot.message_handler(commands=['argmax'])
+def send_max_number_index(message):
+	integer_string= bot.send_message(message.chat.id, 'یک رشته از اعداد به این صورت وارد نمایید: \n 1,2,3,4,5,6')
+	bot.register_next_step_handler(integer_string, find_max_index)
+
+#### FIND_INDEX_OF_MAX_NUMBER
+
+def find_max_index(numbers_string):
+    numbers_string_list = numbers_string.text.split(',')
+    max_number = max(numbers_string_list)
+    max_index = numbers_string_list.index(max_number)
+    bot.send_message(numbers_string.chat.id , max_index+1)
+
 ##################
 
 @bot.message_handler(commands=['qrcode'])
@@ -74,11 +116,13 @@ def send_qrcodemaker(message):
 	qr_message= bot.send_message(message.chat.id , 'متنی وارد کنید تا برای شما بصورت qrcode نمایش داده شود')
 	bot.register_next_step_handler(qr_message,qrcode_show)
 
-def qrcode_show(message):
-    img= qrcode.make(message.text)
-    img.save('qrcode1.png')
-    photo = open('qrcode1.png', 'rb')
-    bot.send_photo(message , photo)
+###### SEND_QRCODE_IMAGE_OF_A_TEXT
+
+def qrcode_show(qr_message):
+    img= qrcode.make(qr_message.text)
+    img.save('qrcode.png')
+    photo = open('qrcode.png', 'rb')
+    bot.send_photo(qr_message.chat.id , photo)
 
 bot.infinity_polling()
 
